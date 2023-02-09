cac753a7fe176303e85583f15f3d670f7080437f
mzhan59 <mzhan59@gmail.com>
2023-02-09 12:07:49 -0500

    passing es6 tests

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,23 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this;
+  }
+
+  enqueue(value) {
+    let key = Object.keys(this)[Object.keys(this).length - 1] + 1;
+    this[key] = value;
+
+  }
+
+  dequeue() {
+    let item = this[Object.keys(this)[0]];
+    delete this[Object.keys(this)[0]];
+    return item;
+  }
+
+  size() {
+    return Object.keys(this).length;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this;
   }
+
+
+  push(value) {
+    let key = Object.keys(this).length + 1;
+    this[key] = value;
+
+  }
+
+  pop() {
+    let item = this[Object.keys(this).length];
+    delete this[Object.keys(this).length];
+    return item;
+  }
+
+  size() {
+    return Object.keys(this).length;
+  }
+
 
 }
